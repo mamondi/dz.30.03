e45ed6ab89c1dafe3718c237725b1cc5ec962d45
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-04 11:52:21 +0300

    done

---
+++ dz.30.03/Del.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Del
+{
+    internal class Del
+    {
+        internal delegate bool FilterDelegate(int number); 
+
+        public List<int> FilterNumbers(int[] numbers, FilterDelegate filter)
+        {
+            List<int> result = new List<int>();
+            foreach (int number in numbers)
+            {
+                if (filter(number))
+                {
+                    result.Add(number);
+                }
+            }
+            return result;
+        }
+
+        public bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+
+        public bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public List<int> GetFibonacciNumbers(int[] numbers)
+        {
+            List<int> result = new List<int>();
+            foreach (int number in numbers)
+            {
+                if (IsFibonacci(number))
+                {
+                    result.Add(number);
+                }
+            }
+            return result;
+        }
+
+        public bool IsFibonacci(int number)
+        {
+            return IsPerfectSquare(5 * number * number + 4) || IsPerfectSquare(5 * number * number - 4);
+        }
+
+        public bool IsPerfectSquare(int number)
+        {
+            int sqrt = (int)Math.Sqrt(number);
+            return sqrt * sqrt == number;
+        }
+    }
+}
--- dz.30.03/Program.cs
+++ dz.30.03/Program.cs
@@ -1,1 +1,49 @@
-﻿
+﻿using System;
+
+namespace dz30._03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+            Del.Del del = new Del.Del();
+
+            Console.WriteLine("Парні числа:");
+            var evens = del.FilterNumbers(numbers, del.IsEven);
+            foreach (var number in evens)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Непарні числа:");
+            var odds = del.FilterNumbers(numbers, del.IsOdd);
+            foreach (var number in odds)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Прості числа:");
+            var primes = del.FilterNumbers(numbers, del.IsPrime);
+            foreach (var number in primes)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Числа Фібоначчі:");
+            var fibonacci = del.GetFibonacciNumbers(numbers);
+            foreach (var number in fibonacci)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
