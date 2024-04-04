67f79aa054065bc22421067486ee390fc7646f19
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-04 12:10:51 +0300

    done

--- dz.30.03/Program.cs
+++ dz.30.03/Program.cs
@@ -1,28 +1,21 @@
 ﻿using System;
 
 namespace dz30._03
-{   internal class Program
+{
+    internal class Program
     {
         static void Main(string[] args)
         {
             Console.InputEncoding = System.Text.Encoding.UTF8;
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            CreditCard creditCard = new CreditCard("1234567890123456", "John Doe", "12/25", "1234", 5000, 1000);
+            MessageDisplay messageDisplay = new MessageDisplay();
 
-            creditCard.TopUp += () => Console.WriteLine("Рахунок поповнено");
-            creditCard.Spend += () => Console.WriteLine("Кошти знято з рахунку");
-            creditCard.CreditStarted += () => Console.WriteLine("Розпочато використання кредитних коштів");
-            creditCard.LimitReached += () => Console.WriteLine("Досягнуто ліміт кредитних коштів");
-            creditCard.PinChanged += () => Console.WriteLine("PIN успішно змінено");
-
-            creditCard.DisplayCardInfo();
-
-            creditCard.TopUpMoney(2000);
-            creditCard.SpendMoney(500);
-            creditCard.StartCredit();
-            creditCard.SpendMoney(3000);
-            creditCard.ChangePin("4321");
+            messageDisplay.DisplayMessage("Це звичайне повідомлення.");
+            messageDisplay.DisplayMessageWithPrefix("Увага:", "Це повідомлення має префікс.");
+            messageDisplay.DisplayMessageInUppercase("Це повідомлення у верхньому регістрі.");
+            Console.WriteLine();
+            messageDisplay.DisplayCustomMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla blandit tristique enim, id vehicula lorem ultrices vitae. Ut volutpat orci eget purus porttitor, quis vulputate dolor elementum. Aliquam lacus arcu, euismod at semper eget, fermentum nec nunc. Sed vitae enim lectus. Phasellus dictum ac ligula nec egestas.");
         }
     }
 }
