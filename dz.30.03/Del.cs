b0f4a6fe4a4fba0d7edd3640ab93ead0cd7fdbb6
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-04 12:03:37 +0300

    done

---
+++ dz.30.03/Del.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Project
+{
+    internal class Del
+    {
+        public void DisplayCurrentTime()
+        {
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+        }
+
+        public void DisplayCurrentDate()
+        {
+            Console.WriteLine(DateTime.Today.ToShortDateString());
+        }
+
+        public void DisplayCurrentDayOfWeek()
+        {
+            Console.WriteLine(DateTime.Today.DayOfWeek);
+        }
+
+        public double CalculateTriangleArea(double baseLength, double height)
+        {
+            return 0.5 * baseLength * height;
+        }
+
+        public double CalculateRectangleArea(double length, double width)
+        {
+            return length * width;
+        }
+    }
+}
--- dz.30.03/Program.cs
+++ dz.30.03/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+namespace Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Del del = new Del();
+
+            Console.WriteLine("Поточний час:");
+            del.DisplayCurrentTime();
+
+            Console.WriteLine("Поточна дата:");
+            del.DisplayCurrentDate();
+
+            Console.WriteLine("Поточний день тижня:");
+            del.DisplayCurrentDayOfWeek();
+
+            double triangleArea = del.CalculateTriangleArea(5, 7);
+            Console.WriteLine($"Площа трикутника зі сторонами 5 та 7: {triangleArea}");
+
+            double rectangleArea = del.CalculateRectangleArea(4, 8);
+            Console.WriteLine($"Площа прямокутника зі сторонами 4 та 8: {rectangleArea}");
+        }
+    }
+}
